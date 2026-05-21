c0c1f1d151bb5c00140bd6321c6b98c282c9520d
ohmydevops <ohmydevops@gmail.com>
2026-05-21 14:30:49 +0330

    refactor: add vite-plugin-singlefile to package.json and configure in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
   root: 'src',
   publicDir: 'public',
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), viteSingleFile()],
   build: {
     outDir: '../dist',
     emptyOutDir: true,
